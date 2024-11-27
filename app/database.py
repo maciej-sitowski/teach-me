f28f4f2d3019d08edd0d7e28783e4e8b49b6b67c
Maciej Sitowski <hbd@MS-WORK>
2024-11-27 21:43:07 +0100

    switch to fastapi

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 from sqlmodel import SQLModel, create_engine, Session
+import os
 
-DATABASE_URL = "postgresql://myuser:mypassword@localhost/mydatabase"
+# DATABASE_URL = "postgresql://myuser:mypassword@localhost/mydatabase"
+
+DATABASE_URL = f"postgresql://postgres.qtbtxfapbpkvormweems:{os.environ['DATABASE_PASSWORD']}@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(DATABASE_URL)
 
